be194960cc30cff8c4f4da04339e97240b49c032
nicolk <69297601+nicolk@users.noreply.github.com>
2026-05-08 18:16:46 +0200

    Update CrossBorderPaymentView.vue

--- src/views/CrossBorderPaymentView.vue
+++ src/views/CrossBorderPaymentView.vue
@@ -232,8 +232,8 @@ const { navigate } = useUI()
 /* demo image */
 
 .demo-image {
-  width: 608px;
-  height: 429px;
+  width: 548px;
+  height: 379px;
   background: url('@/assets/images/cross-border-pay-demo-image.png') left center no-repeat;
 }
 
